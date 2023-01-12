7d1884b80b4cde910e88149f0eba939a8717a052
JMAgstn <119966783+JMAgstn@users.noreply.github.com>
2023-01-12 17:12:55 +0800

    before refactor

--- server.js
+++ server.js
@@ -9,9 +9,8 @@ const DB = process.env.DATABASE.replace(
 );
 
 mongoose
-  .connect(process.env.DATABASE_LOCAL, {
-    /* })
-  .connect(DB, { */
+  // .connect(process.env.DATABASE_LOCAL, { //connecting to local host
+  .connect(DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -20,6 +19,31 @@ mongoose
     console.log('DB connections successful');
   });
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: { type: Number, default: 4.5 },
+  price: { type: Number, required: [true, 'A tour must have a price'] },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+const testTour = new Tour({
+  name: 'The Sea Explorer',
+  rating: 4.7,
+  price: 497,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('ERROR', err);
+  });
 //console.log(app.get('env')); //shows environment
 /* console.log(process.env); */
 
