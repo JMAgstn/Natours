3a72edb8e79cbab294f4409c4b48969e2832cc2d
JMAgstn <119966783+JMAgstn@users.noreply.github.com>
2023-01-12 20:03:37 +0800

    updated tourModel

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -23,18 +23,34 @@ const Tour = require('./../models/tourModel');
   next();
 }; */
 
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    staus: 'success',
-    //data: { tours },
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      staus: 'success',
+      data: { tours },
+    });
+  } catch (err) {
+    res.status(404).body.json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
-exports.getTour = (req, res) => {
-  // const tour = tours[req.params.id * 1];
-  res.status(200).json({
-    status: 'success',
-    // data: { tour: tour },
-  });
+
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: { tour: tour },
+    });
+  } catch (err) {
+    res.status(404).body.json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
@@ -52,13 +68,22 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  // const tour = tours[req.params.id * 1];
-  res.status(200).json({
-    status: 'success',
-    message: 'Updated tour',
-    // data: { tour: tour },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: 'success',
+      data: { tour: '<Updated Tour here...>' },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 exports.deleteTour = (req, res) => {
