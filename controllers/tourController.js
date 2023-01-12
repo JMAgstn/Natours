7196730c5764671664193f6af2e9c2894b792d21
JMAgstn <119966783+JMAgstn@users.noreply.github.com>
2023-01-12 20:23:38 +0800

    added deletemodel

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -86,11 +86,17 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  // const tour = tours[req.params.id * 1];
-  res.status(200).json({
-    status: 'success',
-    message: 'Deleted this tour',
-    // data: { tour: tour },
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      //data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
