9b4dd6ac53cf2d454818de33dcbdf53ee67cdc47
JMAgstn <119966783+JMAgstn@users.noreply.github.com>
2023-01-12 14:10:08 +0800

    successful connection to db

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connections successful');
+  });
 //console.log(app.get('env')); //shows environment
 /* console.log(process.env); */
 
