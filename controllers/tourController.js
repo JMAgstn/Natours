cb2174e0cd941cb3da2d931e5e34890849f6c151
JMAgstn <119966783+JMAgstn@users.noreply.github.com>
2023-01-12 18:09:17 +0800

    updated createTour model

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -13,7 +13,7 @@ const Tour = require('./../models/tourModel');
   next();
 }; */
 
-exports.checkBody = (req, res, next) => {
+/* exports.checkBody = (req, res, next) => {
   if (!('name' in req.body) || !('price' in req.body)) {
     res.status(400).json({
       status: 'fail',
@@ -21,7 +21,7 @@ exports.checkBody = (req, res, next) => {
     });
   }
   next();
-};
+}; */
 
 exports.getAllTours = (req, res) => {
   res.status(200).json({
@@ -37,11 +37,19 @@ exports.getTour = (req, res) => {
   });
 };
 
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    // data: { tour: newTour },
-  });
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: { tour: newTour },
+    });
+  } catch (err) {
+    res.status(400).body.json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
